9704bbf103ad0145152a3e72bf30b73a182f1fa1
eduinbello19 <eduinbelloj19@gmail.com>
2026-07-10 18:11:03 -0500

    Se realiza la codificacion de la Conexion.java con la DB

---
+++ src/connection/Conexion.java
@@ -0,0 +1,25 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion
+{
+    public Connection getConexion()
+    {
+        Connection con = null;
+
+        try
+        {
+            //asi es en nuestro caso
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/alexa_nails_app","root","");
+            //en el caso del mack el XAMP corre por el 8889
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return  con;
+    }
+}
