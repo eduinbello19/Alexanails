77c18bce5f0077f32206670b38745ba7e0d97841
eduinbello19 <eduinbelloj19@gmail.com>
2026-07-10 21:51:44 -0500

    creacion de getters en Cita.java por que estaban causando errores criticos

--- src/model/Cita.java
+++ src/model/Cita.java
@@ -2,7 +2,7 @@ package model;
 
 public class Cita
 {
-    //Atributos
+    // Atributos
     private int id_cita;
     private String fecha;
     private String hora;
@@ -13,8 +13,8 @@ public class Cita
 
     // Constructor
     public Cita(int id_cita, String fecha, String hora, String estado,
-                int id_cliente, int id_empleado, int id_servicio) {
-
+                int id_cliente, int id_empleado, int id_servicio)
+    {
         this.id_cita = id_cita;
         this.fecha = fecha;
         this.hora = hora;
@@ -22,5 +22,77 @@ public class Cita
         this.id_cliente = id_cliente;
         this.id_empleado = id_empleado;
         this.id_servicio = id_servicio;
+    }
 
+    // Getters
+    public int getId_cita()
+    {
+        return id_cita;
+    }
+
+    public String getFecha()
+    {
+        return fecha;
+    }
+
+    public String getHora()
+    {
+        return hora;
+    }
+
+    public String getEstado()
+    {
+        return estado;
+    }
+
+    public int getId_cliente()
+    {
+        return id_cliente;
+    }
+
+    public int getId_empleado()
+    {
+        return id_empleado;
+    }
+
+    public int getId_servicio()
+    {
+        return id_servicio;
+    }
+
+    // Setters
+    public void setId_cita(int id_cita)
+    {
+        this.id_cita = id_cita;
+    }
+
+    public void setFecha(String fecha)
+    {
+        this.fecha = fecha;
+    }
+
+    public void setHora(String hora)
+    {
+        this.hora = hora;
+    }
+
+    public void setEstado(String estado)
+    {
+        this.estado = estado;
+    }
+
+    public void setId_cliente(int id_cliente)
+    {
+        this.id_cliente = id_cliente;
+    }
+
+    public void setId_empleado(int id_empleado)
+    {
+        this.id_empleado = id_empleado;
+    }
+
+    public void setId_servicio(int id_servicio)
+    {
+        this.id_servicio = id_servicio;
+    }
 }
