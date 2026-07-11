16a935a041fbf29f46d30cc5508c41532a6c7b84
eduinbello19 <eduinbelloj19@gmail.com>
2026-07-10 20:53:56 -0500

    metodo eactualizar

--- src/controller/CitaController.java
+++ src/controller/CitaController.java
@@ -80,4 +80,40 @@ public class CitaController
     }
     //metodo actualizar
 
+    public void actualizar(Cita cita)
+    {
+        Connection con = conexion.getConexion();
+
+        String query = "UPDATE cita SET fecha = ?, hora = ?, estado = ?, id_cliente = ?, id_empleado = ?, id_servicio = ? WHERE id_cita = ?";
+
+        try
+        {
+            PreparedStatement pst = con.prepareStatement(query);
+
+            pst.setString(1, cita.getFecha());
+            pst.setString(2, cita.getHora());
+            pst.setString(3, cita.getEstado());
+            pst.setInt(4, cita.getId_cliente());
+            pst.setInt(5, cita.getId_empleado());
+            pst.setInt(6, cita.getId_servicio());
+
+            pst.setInt(7,cita.getId_cita());
+
+            int resultado = pst.executeUpdate();
+
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "Cita actualizado correctamente.");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Hubo un error al actualizar la cita.");
+            }
+
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
